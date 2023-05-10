eb4010de1fde2f71a6bd73a9b83e60ff9471cf4a
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-05-09 23:45:46 -0400

    vistaPizza

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    var input = document.getElementById("mejoraPizza");
+    input.value = "MEJORA TU PIZZA";
+
+    var input = document.getElementById("resumenPedido");
+    input.value = "RESUMEN DEL PEDIDO";
+};
